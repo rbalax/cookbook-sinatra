1636951b838da4326066685facefa377036e4de9
rom balax <rbalax@yahoo.co.uk>
2021-08-01 11:43:37 +0900

    Sinatra step 1 done

--- app.rb
+++ app.rb
@@ -7,6 +7,18 @@ configure :development do
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
+
+
+get '/about' do
+  erb :about
+end
+
+get '/team/:username' do
+  puts params[:username]
+  "The username is #{params[:username]}"
+end
+
 get '/' do
-  'Hello Rom!'
+  @usernames = [ 'ssaunier', 'Papillard', 'Romu' ]
+  erb :index
 end
